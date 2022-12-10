ef5b425d058b691dc457d8fd28260d03652eb7ee
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-10 23:47:05 +0200

    HW task 2 solved

--- HomeWork/task2/Program.cs
+++ HomeWork/task2/Program.cs
@@ -3,4 +3,37 @@
 // не использовать для получения цифр числа операцию приведения числа к строке
 
 // проверить и сразу прибавить к сумме 
-// проверить и составить массив, а потом сложить цифры массива 
+
+int GetLengthOfNumber(int val)
+{
+    int count = 0; // количество цифр
+    while(val > 0) // сначала определись с действием, потом услови 
+    {   
+        val = val / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int lastDigit = N % 10;
+int Nremain = N; // N remain — остающееся после отбрасывания последней цифры число
+int length = GetLengthOfNumber(N); // количество цифр в числе
+
+for (int i = 0; i < length; i++) // перебор всех цифр
+{
+    if(lastDigit != 0) // на случай, если цифра будет 0, делить на 0 нельзя
+    {
+        if(N % lastDigit == 0) // проверка на соответствие характеристике делителя
+        {
+            sum = sum + lastDigit; // добавляем в сумму 
+        }
+        Nremain = Nremain / 10; // отбрасываем последнюю цифру
+        lastDigit = Nremain % 10; // находим последнюю цифру нового числа 
+    }
+}
+Console.WriteLine("Сумма всех цифр-делителей введённого числа: " + sum);
+Console.WriteLine();
+
